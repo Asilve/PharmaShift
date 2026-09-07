c7bcc97ef58c8045d35b21ad43d4375b3c1dd7d7
Anthony Silvester <asilvester196@gmail.com>
2026-09-07 18:30:35 +0100

    add home screen ui

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = QMainWindow()
-    window.setWindowTitle("Pharma-Shift")
-    window.resize(1200, 700)
+    loader = QUiLoader()
+    window = loader.load("ui_files/home.ui", None)
+
+    window.setWindowTitle("Pharma Shift")
+    window.resize(1280, 720)
 
     window.show()
-
     sys.exit(app.exec())
 
 
