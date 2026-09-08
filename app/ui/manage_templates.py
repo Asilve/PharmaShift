ac354365b490fef38ab6a7b95da3cb9974e70dae
Anthony Silvester <asilvester196@gmail.com>
2026-09-08 20:21:28 +0100

    implement select card functionality

--- app/ui/manage_templates.py
+++ app/ui/manage_templates.py
@@ -1,7 +1,6 @@
-from PySide6.QtCore import Signal, Qt
+from PySide6.QtCore import QEvent, Qt, Signal
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLayout
-from PySide6.QtWidgets import QApplication
 
 from models.template import EmployeeTemplate
 from ui.template_card import TemplateCard
@@ -16,22 +15,39 @@ class ManageTemplatesPage(QWidget):
 
         self.database = database
 
+        # Card selection
+        self.selected_card = None
+
         loader = QUiLoader()
         self.ui = loader.load("ui_files/manage_templates.ui", None)
 
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.ui)
+        self.ui.scrollArea.viewport().installEventFilter(self)
 
+        # Event filters for clearing selection
+        self.installEventFilter(self)
+        self.ui.installEventFilter(self)
+        self.ui.scrollArea.viewport().installEventFilter(self)
+
+        # Back Button
         self.back_button = self.ui.findChild(QPushButton,"back_button")
-
         self.back_button.clicked.connect(self.back_clicked.emit)
 
+        # Layout template list
         self.template_list_layout = self.ui.findChild(QLayout,"template_list_layout")
-
         self.template_list_layout.setAlignment(Qt.AlignTop)
 
+        # Buttons
+        self.edit_button = self.ui.findChild(QPushButton, "edit_button")
+        self.delete_button = self.ui.findChild(QPushButton, "delete_button")
+        self.edit_button.setEnabled(False)
+        self.delete_button.setEnabled(False)
+
         self.load_templates()
+
+        
 
     def load_templates(self):
         templates = self.database.get_templates()
@@ -41,4 +57,38 @@ class ManageTemplatesPage(QWidget):
 
             card = TemplateCard(template)
 
+            card.clicked.connect(lambda checked=False, card=card: self.select_template(card))
+
             self.template_list_layout.addWidget(card,0,Qt.AlignHCenter | Qt.AlignTop)
+
+
+    def select_template(self, card):
+        if self.selected_card is not None:
+            self.selected_card.set_selected(False)
+
+        self.selected_card = card
+        self.selected_card.set_selected(True)
+
+        self.edit_button.setEnabled(True)
+        self.delete_button.setEnabled(True)
+
+
+    def eventFilter(self, watched, event):
+        if event.type() == QEvent.MouseButtonPress:
+            if watched == self.ui.scrollArea.viewport():
+                self.clear_selection()
+
+        return super().eventFilter(watched, event)
+
+    def clear_selection(self):
+        if self.selected_card is not None:
+            self.selected_card.set_selected(False)
+            self.selected_card = None
+
+        self.edit_button.setEnabled(False)
+        self.delete_button.setEnabled(False)
+
+    def mousePressEvent(self, event):
+        self.clear_selection()
+        super().mousePressEvent(event)
+
--- app/ui/template_card.py
+++ app/ui/template_card.py
@@ -1,10 +1,14 @@
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame
+from PySide6.QtCore import Qt, Signal
 
 from models.template import EmployeeTemplate
 
 
 class TemplateCard(QWidget):
+
+    clicked = Signal()
+
     def __init__(self, template: EmployeeTemplate):
         super().__init__()
 
@@ -12,6 +16,7 @@ class TemplateCard(QWidget):
 
         loader = QUiLoader()
         self.ui = loader.load("ui_files/template_card.ui")
+        self.ui.setObjectName("template_card")
 
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
@@ -19,9 +24,7 @@ class TemplateCard(QWidget):
 
 
         self.name_label = self.ui.findChild(QLabel,"name_label")
-
         self.subtitle_label = self.ui.findChild(QLabel,"subtitle_label")
-
         self.colour_indicator = self.ui.findChild(QFrame,"colour_indicator")
 
         self.set_template(template)
@@ -31,7 +34,17 @@ class TemplateCard(QWidget):
 
         self.name_label.setText(template.name)
         self.subtitle_label.setText("Pharmacist Template")
+        self.colour_indicator.setStyleSheet(f"background-color: {template.colour};")
 
-        self.colour_indicator.setStyleSheet(
-            f"background-color: {template.colour};"
-        )
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.clicked.emit()
+
+
+    def set_selected(self, selected):
+        self.selected = selected
+        self.ui.setProperty("selected", "true" if selected else "false")
+
+        stylesheet = self.ui.styleSheet()
+        self.ui.setStyleSheet("")
+        self.ui.setStyleSheet(stylesheet)
